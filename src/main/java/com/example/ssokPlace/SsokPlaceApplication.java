d549bb01337ebc762e44a0e802aa1b0c3bfc2b81
OneHoWon <flash0221@ajou.ac.kr>
2025-09-10 12:57:58 +0900

    chore: First Commit

---
+++ src/main/java/com/example/ssokPlace/SsokPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssokPlace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsokPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsokPlaceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ssokPlace/SsokPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssokPlace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsokPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
