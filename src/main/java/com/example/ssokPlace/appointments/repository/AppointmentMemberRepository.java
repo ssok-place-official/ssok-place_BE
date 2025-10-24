bde3deca844a47547c073aacf3c9a384808c8593
onehowon <flash0221@naver.com>
2025-10-24 14:03:27 +0900

    feat: Repository 및 쿼리 작성

---
+++ src/main/java/com/example/ssokPlace/appointments/repository/AppointmentMemberRepository.java
@@ -0,0 +1,10 @@
+package com.example.ssokPlace.appointments.repository;
+
+import com.example.ssokPlace.appointments.entity.AppointmentMember;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface AppointmentMemberRepository extends JpaRepository<AppointmentMember, Long> {
+    Optional<AppointmentMember> findByAppointment_PublicIdAndUser_Id(String apptPublicId, Long userId);
+}
