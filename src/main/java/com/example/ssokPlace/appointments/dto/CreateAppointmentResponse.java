7712ac7b338465d14841930a30ec4520446905d0
onehowon <flash0221@naver.com>
2025-11-03 16:49:49 +0900

    fix: placeId가 안넘어오는 오류가 있으나 추후 수정

--- src/main/java/com/example/ssokPlace/appointments/dto/CreateAppointmentResponse.java
+++ src/main/java/com/example/ssokPlace/appointments/dto/CreateAppointmentResponse.java
@@ -8,21 +8,25 @@ import java.util.List;
 
 public class CreateAppointmentResponse {
     private final Long id;
+    private final Long placeId;
     private final AppointmentsStatus status;
     private final List<MemberDto> members;
 
     @JsonCreator
     public CreateAppointmentResponse(
             @JsonProperty("id") Long id,
+            @JsonProperty("placeId") Long placeId,
             @JsonProperty("status") AppointmentsStatus status,
             @JsonProperty("members") List<MemberDto> members
     ) {
         this.id = id;
+        this.placeId = placeId;
         this.status = status;
         this.members = members;
     }
 
     public Long getId() { return id; }
-    public AppointmentsStatus getStatus() { return status; }
+    public AppointmentsStatus getStatus() { return status;}
+    public Long getPlaceId() { return placeId; }
     public List<MemberDto> getMembers() { return members; }
 }
